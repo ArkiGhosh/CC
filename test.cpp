59c6b56a98b6250d91db0e1fb0e95b66bf014b61
Arkishman Ghosh <arkighosh0807@gmail.com>
2022-01-30 16:11:59 +0530

    temp

--- test.cpp
+++ test.cpp
@@ -3,49 +3,15 @@ using namespace std;
 #define int long long 
 #define endl "\n"
 #define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
 //type cast .size() to int
 //stay calm
-
-vector<vector<int>> lis;
-vector<int> visited, length;
-
-void addedge(int u, int v){
-    lis[u].push_back(v); lis[v].push_back(u);
-}
-
-void dfs(int v, int p){
-    visited[v] = 1;
-    for(int i : lis[v]){
-        if(i != p){
-            length[i] = length[v] + 1;
-            dfs(i, v);
-        }
-    }
-}
 
 void solve()
 {
     int n, m; cin >> n >> m;
-    lis.assign(n, vector<int>());
-    visited.assign(n, 0);
-    length.assign(n, 0);
-    int u, v;
-    for(int i = 0; i < m; i++){
-        cin >> u >> v;
-        u--; v--;
-        addedge(u, v);
-    }
-    dfs(0, -1);
-    vector<pair<int, int>> ans;
-    for(int i = 0; i < n; i++){
-        ans.push_back({length[i], i});
-    }
-    sort(ans.begin(), ans.end());
-    int i = ans[n - 1].second;
-    for(int j = 0; j < n; j++) length[j] = 0;
-    dfs(i, -1);
-    sort(length.begin(), length.end());
-    cout << length[n - 1] << endl;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
 }
 
 int32_t main()
