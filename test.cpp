873e255134a138e08aeae5bc83e24bf0c2f67b84
Arkishman Ghosh <arkighosh0807@gmail.com>
2022-02-07 00:21:35 +0530

    test

--- test.cpp
+++ test.cpp
@@ -4,14 +4,12 @@ using namespace std;
 #define endl "\n"
 #define sz(x) (int)(x).size()
 #define all(x) x.begin(), x.end()
-//type cast .size() to int
+#define len(x) (int)(x).length()
 //stay calm
 
 void solve()
 {
-    int n, m; cin >> n >> m;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
+    int n; cin >> n;
 }
 
 int32_t main()
@@ -19,8 +17,9 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    //int t; cin >> t;
-    //while (t--) 
+    int t = 1; 
+    cin >> t;
+    while (t--) 
     solve();
     return 0;
 }
